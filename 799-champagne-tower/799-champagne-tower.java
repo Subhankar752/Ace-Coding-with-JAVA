1b542c367722b6ea4c152fe90b186d552bdebff3
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-03-04 09:46:57 +0530

    Time: 2 ms (99.54%), Space: 42.4 MB (71.69%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.java
+++ 799-champagne-tower/799-champagne-tower.java
@@ -6,7 +6,7 @@ class Solution {
         dp[0][0] = k;
         
         for(int i = 0 ; i < r ; i++){
-            for(int j = 0 ; j <= c ;  j++){
+            for(int j = 0 ; j < c + 1 ;  j++){
                 if(dp[i][j] > 1.0){
                     double spare = dp[i][j] - 1;
                     spare /= 2;
