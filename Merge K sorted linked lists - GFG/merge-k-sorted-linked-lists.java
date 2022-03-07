fd15f6eb8baf712bbea2f8527a994331efb39c6e
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-03-07 09:26:35 +0530

    Added solution - LeetHub

---
+++ Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.java
@@ -0,0 +1,108 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+
+class GfG
+{
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {   
+            int N = sc.nextInt();
+            
+            Node []a = new Node[N];
+            
+            for(int i = 0; i < N; i++)
+            {
+                int n = sc.nextInt();
+                
+                Node head = new Node(sc.nextInt());
+                Node tail = head;
+                
+                for(int j=0; j<n-1; j++)
+                {
+                    tail.next = new Node(sc.nextInt());
+                    tail = tail.next;
+                }
+                
+                a[i] = head;
+            }
+            
+            Solution g = new Solution();
+             
+            Node res = g.mergeKList(a,N);
+            if(res!=null)
+                printList(res);
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+/*class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+// a is an array of Nodes of the heads of linked lists
+// and N is size of array a
+
+
+class Solution
+{
+    //Function to merge K sorted linked list.
+    Node mergeKList(Node[]a,int N)
+    {
+        //Add your code here.
+     ArrayList<Integer> b = new ArrayList<Integer>();
+     for(int i = 0 ; i < N ; i++)
+     {
+         Node temp = a[i];
+         while(temp != null)
+         {
+             b.add(temp.data);
+             temp = temp.next;
+         }
+     }
+     Collections.sort(b);
+     Node head = new Node(b.get(0));
+     Node temp = head;
+     for(int i = 1 ; i < b.size() ; i++)
+     {
+         temp.next  = new Node(b.get(i));
+         temp = temp.next;
+     }
+    return head;
+    }
+}
