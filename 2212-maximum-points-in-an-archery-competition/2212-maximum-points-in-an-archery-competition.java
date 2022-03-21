849676b53cf415403f86b29caa4a7d66f090f126
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-03-21 11:49:47 +0530

    Time: 818 ms (50.00%), Space: 219.6 MB (50.00%) - LeetHub

--- 2212-maximum-points-in-an-archery-competition/2212-maximum-points-in-an-archery-competition.java
+++ 2212-maximum-points-in-an-archery-competition/2212-maximum-points-in-an-archery-competition.java
@@ -1,24 +1,55 @@
 class Solution {
-    int mx = 0;
-    int[] ans = null;
     public int[] maximumBobPoints(int numArrows, int[] aliceArrows) {
-        dfs(aliceArrows, new int[12], numArrows, 0, 0);
-        return ans;
-    }
-    
-    void dfs(int[] a, int b[], int t, int p, int id) {
-        if (t < 0) return;
-        if (p > mx) {
-            mx = p;
-            int[] nb1 = Arrays.copyOf(b, 12);
-            if (t > 0) nb1[0] += t;
-            ans = nb1;
+        
+        
+        
+        //filling dp table like knapsack
+        int dp[][]=new int[numArrows+1][aliceArrows.length+1];
+        
+        for(int i=1;i<=numArrows;i++){
+            
+            for(int j=1;j<aliceArrows.length;j++){
+                
+                
+                dp[i][j]=dp[i][j-1];
+                if(i>=aliceArrows[j]+1){
+                    
+                    dp[i][j]=Math.max(dp[i-aliceArrows[j]-1][j-1]+j,dp[i][j]);
+                }
+                
+            }
         }
         
-        for(int i=id; i<a.length; i++) {
-            int[] nb = Arrays.copyOf(b, 12);
-            nb[i] = a[i]+1;
-            dfs(a, nb, t-nb[i], p+i, i+1);
+        
+        int ans[]=new int[aliceArrows.length];
+        
+        int i=numArrows;
+        int j=aliceArrows.length-1;
+        
+        //backtracking to fill the arrows shot by bob;
+        int sum=0;
+        for(int k=aliceArrows.length-1;k>0;k--){
+            
+            
+            if(i>=aliceArrows[j]+1 && dp[i-aliceArrows[j]-1][j-1]+j>dp[i][j-1]){
+                
+                ans[k]=aliceArrows[k]+1;
+                i=i-aliceArrows[j]-1;
+                j=j-1;
+            }
+            else{
+                ans[k]=0;
+                j=j-1;
+            }
+            
+            sum+=ans[k];
         }
+        
+        
+        //throwing remaining arrows at 0th target.
+        ans[0]=numArrows-sum;
+        
+        
+        return ans;
     }
 }
