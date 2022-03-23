76a8c5e50ec5b79eebcda9db6d5684391543d693
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-03-23 08:40:32 +0530

    Added solution - LeetHub

---
+++ Swap Kth nodes from ends - GFG/swap-kth-nodes-from-ends.java
@@ -0,0 +1,165 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+
+class LinkedList
+{
+    static  Node head;  
+    static  Node lastNode;
+    
+    public static void addToTheLast(Node node)
+    {
+
+        if (head == null)
+        {
+            head = node;
+            lastNode = node;
+        }
+        else
+        {
+            Node temp = head;
+            lastNode.next = node;
+            lastNode = node;
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        Scanner sc=  new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n, K;
+            n = sc.nextInt();
+            K = sc.nextInt();
+            
+            Node head = null;
+            int val = sc.nextInt();
+            head = new Node(val);
+            addToTheLast(head);
+            
+            for(int i = 1; i< n; i++)
+            {
+                val = sc.nextInt();
+                addToTheLast(new Node(val));
+            }
+            
+            Node before[] = new Node[n];
+            addressstore(before, head);
+            GFG obj = new GFG();
+            
+            head = obj.swapkthnode(head, n, K);
+        
+           Node after[] = new Node[n];
+          addressstore(after, head);
+        
+        if(check(before, after, n, K) == true)
+            System.out.println("1");
+        else
+            System.out.println("0");
+        
+        }
+    }
+    
+    static boolean check(Node before[], Node after[], int num, int K)
+    {
+          if(K > num)
+           return true;
+           
+           return (before[K-1] == after[num - K]) && (before[num-K] == after[K-1]);
+              
+       
+    }
+    
+    static void addressstore(Node arr[], Node head)
+  {
+      Node temp = head;
+      int i = 0;
+      while(temp != null){
+        arr[i] = temp;
+        i++;
+        temp = temp.next;
+    }
+}
+    
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+/* Linked List Node class
+   class Node  {
+     int data;
+     Node next;
+     Node(int data)
+     {
+         this.data = data;
+         next = null;
+     }
+  }
+*/
+class GFG
+{
+    //Function to swap Kth node from beginning and end in a linked list.
+    Node swapkthnode(Node head, int num, int K)
+    {
+        // your code here
+        Node start = head;
+
+if(head==null) return null;
+
+if(head.next == null || K>num){
+return head;
+}
+int c = 1;
+while(start!=null){
+if(c == K){
+break;
+}
+c++;
+start = start.next;
+}
+Node temp = head;
+Node nextnode = head;
+Node pre = null;
+while(nextnode!=null){
+nextnode = nextnode.next;
+temp.next = pre;
+pre = temp;
+temp = nextnode;
+
+}
+head = pre;
+Node end = head;
+int count = 1;
+while( end != null){
+if(count == K){
+break;
+}
+count++;
+
+end = end.next;
+}
+int a = start.data;
+start.data = end.data;
+end.data = a;
+return head;
+    }
+}
