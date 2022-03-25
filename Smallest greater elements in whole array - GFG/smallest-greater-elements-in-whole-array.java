0fee8ec5f7d4d69c3092374e4cf8bdcbf7c363f6
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-03-25 09:57:08 +0530

    Added solution - LeetHub

---
+++ Smallest greater elements in whole array - GFG/smallest-greater-elements-in-whole-array.java
@@ -0,0 +1,87 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    int sizeOfArray = Integer.parseInt(br.readLine());
+		    int arr [] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Complete obj = new Complete();
+		    arr = obj.greaterElement(arr, sizeOfArray);
+		    for(int i=0; i< sizeOfArray;i++){
+		        if(arr[i] == -10000000)
+		            System.out.print("_ ");
+		        else
+		            System.out.print(arr[i] + " ");
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//User function Template for Java
+
+//User function Template for Java
+
+            
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int[] greaterElement (int arr[], int n) {
+        // Complete the function
+        TreeSet<Integer> set = new TreeSet<Integer>();
+        int res[] = new int[n];
+        
+        for (int i = n - 1; i >= 0; i--) {
+            set.add(arr[i]);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            Integer nextGreater = set.higher(arr[i]);
+            
+            if (nextGreater == null) {
+                res[i] = -10000000;
+            } else {
+                res[i] = (int) nextGreater;
+            }
+        }
+        return res;
+    }
+    
+    
+}
+
+
