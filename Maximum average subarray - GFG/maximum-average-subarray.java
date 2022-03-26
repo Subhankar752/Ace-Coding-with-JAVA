f72ac994cefdb1f6fe2e08c0abf55c4e38b40f8a
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-03-26 22:43:24 +0530

    Added solution - LeetHub

--- Maximum average subarray - GFG/maximum-average-subarray.java
+++ Maximum average subarray - GFG/maximum-average-subarray.java
@@ -36,7 +36,7 @@ class Solution {
     int findMaxAverage(int[] arr, int n, int k) {
         // code here
         int sum = 0;
-        for (int i=0; i<k; i++) {
+        for (int i = 0; i < k; i++) {
             sum += arr[i];
         }
         
