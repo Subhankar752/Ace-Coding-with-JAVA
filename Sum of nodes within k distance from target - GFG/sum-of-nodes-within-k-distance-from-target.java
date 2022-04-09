489578372f4c26eb2b64361297df59cd18aaa193
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-04-09 20:46:15 +0530

    Added solution - LeetHub

---
+++ Sum of nodes within k distance from target - GFG/sum-of-nodes-within-k-distance-from-target.java
@@ -0,0 +1,161 @@
+// { Driver Code Starts
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+	public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int t=Integer.parseInt(br.readLine());
+        
+        while(t > 0){
+            String line = br.readLine().trim();
+            Node root = buildTree(line);
+            
+            line = br.readLine().trim();
+            String target_k[] = line.split(" ");
+            int target = Integer.parseInt(target_k[0]);
+            int k = Integer.parseInt(target_k[1]);
+            
+            solver x = new solver();
+            System.out.println( x.sum_at_distK(root, target, k) );
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+// node structure:
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+*/
+
+class solver{
+    static int sum = 0;
+    static int sum_at_distK(Node root, int target, int k){
+        sum = 0;
+        find(root, target, k);
+        return sum;
+    }
+    
+    static int find(Node root, int target, int k){
+        if(root == null)
+            return -1;
+            
+        if(root.data == target){
+            sum(root, k);
+            return k-1;
+        }  
+        
+        int left = find(root.left, target, k);
+        
+        if(left != -1){
+            sum += root.data;
+            sum(root.right, left-1);
+            return left - 1;
+        }
+        
+        int right = find(root.right, target, k);
+        
+        if(right != -1){
+            sum += root.data;
+            sum(root.left, right-1);
+            return right - 1;
+        }
+        return -1;
+    }
+    
+    static void sum(Node root, int k){
+        if(k < 0 || root == null)
+            return;
+        
+        sum += root.data;
+        sum(root.left, k-1);
+        sum(root.right, k-1);
+    }
+}
+
