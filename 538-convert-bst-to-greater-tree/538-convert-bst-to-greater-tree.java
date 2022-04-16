8dc1475231bc95156e9efc759c2d843be25a42c0
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-04-16 08:48:58 +0530

    Time: 3 ms (13.09%), Space: 42.8 MB (81.93%) - LeetHub

---
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    ArrayList<Integer> list = new ArrayList<>();
+    public TreeNode convertBST(TreeNode root) {
+        inorder(root);
+        HashMap<Integer , Integer> map = new HashMap<>();
+        int sum = 0;
+        int n = list.size();
+        for(int i = n - 1 ; i >=  0 ; i--){
+            sum += list.get(i);
+            map.put(list.get(i) , sum);
+        }
+        solve(root , map);
+        return root;
+    }
+    
+    public void solve(TreeNode root , HashMap<Integer , Integer> map){
+            if(root == null)
+                return;
+        solve(root.left , map);
+        root.val = map.get(root.val);
+        solve(root.right , map);
+    }
+    
+    public void inorder(TreeNode root){
+        if(root == null)
+            return;
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+}
