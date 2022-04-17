ca5d99d71f9207396bd35c3b675e0e6e21393e7a
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-04-17 19:02:02 +0530

    Time: 1 ms, Memory: 40.3 MB - LeetHub

---
+++ decode-string/decode-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> freqStack = new Stack<>();
+        Stack<StringBuilder> strStack = new Stack<>();
+        
+        StringBuilder currStr = new StringBuilder();
+        int k = 0;
+        for(char c : s.toCharArray()){
+            if(Character.isDigit(c)){
+                k = k * 10 + (c - '0');
+            }
+            else if(Character.isLetter(c)){
+                currStr.append(c);
+            }
+            else if(c == '['){
+                freqStack.push(k);
+                strStack.push(currStr);
+                k = 0;
+                currStr = new StringBuilder();
+            }
+            else{
+                StringBuilder temp = currStr;
+                int freq = freqStack.pop();
+                currStr = strStack.pop();
+                while(freq > 0){
+                    currStr.append(temp);
+                    freq--;
+                }
+                k = 0;
+            }
+        }
+        return currStr.toString();
+    }
+}
