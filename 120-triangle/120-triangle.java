8f6331b7dedaf9fc8dd4e3d00d10767d46e35064
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-04-25 12:57:10 +0530

    Time: 4 ms (60.71%), Space: 41.9 MB (86.08%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -2,12 +2,33 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         int[][] dp = new int[n][n];
+        dp[0][0] = triangle.get(0).get(0);
+        
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                dp[i][j] = -1;
+            int m = triangle.get(i).size();
+            for(int j = 0 ; j < m ; j++){
+                dp[i][j] = triangle.get(i).get(j);
             }
         }
-        return solve(triangle , 0 , 0 , dp);
+        
+        for(int i = 1 ; i < n ; i++){
+            dp[i][0] = dp[i][0] + dp[i - 1][0];
+        }
+        for(int i = 1 ; i < n ; i++){
+            int size = triangle.get(i).size();
+            for(int j = 1 ; j < n ; j++){
+                if(j == size - 1){
+                    dp[i][j] = dp[i][j] + dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] = dp[i][j] + Math.min(dp[i - 1][j] , dp[i - 1][j - 1]);
+                }
+            }
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0 ; i < n ; i++)
+            ans = Math.min(ans , dp[n - 1][i]);
+        return ans;
     }
     public int solve(List<List<Integer>> a , int i , int j , int[][] dp){
         if(i == a.size()){
