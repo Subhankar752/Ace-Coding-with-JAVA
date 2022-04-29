1a18ba6470bd180ebb09c2331775c96ee80cb4df
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-04-29 23:09:43 +0530

    Added solution - LeetHub

---
+++ Find length of Loop - GFG/find-length-of-loop.java
@@ -0,0 +1,104 @@
+// { Driver Code Starts
+// driver code
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG
+{
+    public static void makeLoop(Node head, Node tail, int x){
+        if (x == 0) return;
+        
+        Node curr = head;
+        for(int i=1; i<x; i++)
+            curr = curr.next;
+        
+        tail.next = curr;
+    }
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t--> 0)
+        {
+            int n = sc.nextInt();
+            
+            int num = sc.nextInt();
+            Node head = new Node(num);
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                num = sc.nextInt();
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            
+            int pos = sc.nextInt();
+            makeLoop(head, tail, pos);
+            
+            Solution x = new Solution();
+            System.out.println( x.countNodesinLoop(head) );
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+//Function should return the length of the loop in LL.
+
+class Solution
+{
+    //Function to find the length of a loop in the linked list.
+    static int countNodesinLoop(Node head)
+    {
+        //Add your code here.
+        if(head==null && head.next==null)
+return 0;
+Node current=head;
+Node fast=current.next.next;
+Node slow=current.next;
+while(fast!=null && fast.next!=null)
+{
+if(fast==slow)
+return count(slow);
+fast=fast.next.next;
+slow=slow.next;
+}
+return 0;
+}
+static int count(Node slow){
+Node temp=slow;
+int count=1;
+while(temp.next!=slow){
+count++;
+temp=temp.next;
+}
+return count;
+    }
+}
