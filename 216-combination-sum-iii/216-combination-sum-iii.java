f17a6953f182384b37e4e138f89c3068470b657b
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-05-10 10:02:50 +0530

    Time: 1 ms (73.61%), Space: 42 MB (23.95%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+    List<List<Integer>> ans = new ArrayList<>();
+    combination(ans, new ArrayList<Integer>(), k, 1, n , 0);
+    return ans;
+}
+
+private void combination(List<List<Integer>> ans, List<Integer> comb, int k,  int start, int n , int sum) {
+	if (comb.size() == k && n == sum) {
+		List<Integer> li = new ArrayList<Integer>(comb);
+		ans.add(li);
+		return;
+	}
+	for (int i = start; i <= 9; i++) {
+		comb.add(i);
+        sum += i;
+		combination(ans, comb, k, i+1, n , sum);
+		comb.remove(comb.size() - 1);
+        sum -= i;
+	}
+}
+}
