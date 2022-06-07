bee6e95e4199999966d747e6ade6e8d3879a7941
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-06-07 08:40:39 +0530

    Added solution - LeetHub

---
+++ Sorted Link List to BST - GFG/sorted-link-list-to-bst.java
@@ -0,0 +1,136 @@
+// { Driver Code Starts
+import java.util.*;
+
+class LNode
+{
+    int data;
+    LNode next;
+    LNode(int d) {
+        data = d; 
+        next = null;
+    }
+}
+
+class TNode
+{
+    int data;
+    TNode left, right;
+    TNode(int x)
+    {
+        data=x;
+        left=right=null;
+    }
+    
+}
+
+
+class ListToBST
+{
+    
+  /* Function to print linked list */
+   public static void preOrder(TNode root)
+    {
+        
+        if(root==null)
+            return;
+            
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+	
+	 
+ 
+     /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+       
+         
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			LNode head = new LNode(sc.nextInt());
+            LNode tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new LNode(sc.nextInt());
+                tail = tail.next;
+            }
+		
+			Solution obj = new Solution();
+			TNode root = obj.sortedListToBST(head);
+			preOrder(root);
+			
+			System.out.println();
+			
+			t--;
+			
+         }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*Linked List Node
+class LNode
+{
+    int data;
+    LNode next;
+    LNode(int d) {
+        data = d; 
+        next = null;
+    }
+}
+
+//Tree Node
+class TNode
+{
+    int data;
+    TNode left, right;
+    TNode(int x)
+    {
+        data=x;
+        left=right=null;
+    }
+    
+}*/
+
+class Solution
+{
+     public TNode sortedListToBST(LNode head)
+   {
+       if(head == null)
+       return null;
+       
+       LNode midnode = findmid(head);
+      TNode root = new TNode(midnode.data);
+      if(head == midnode)
+      return root;
+       
+       root.left = sortedListToBST(head);
+       root.right = sortedListToBST(midnode.next);
+       return root;
+   }
+   public LNode findmid(LNode head)
+   {
+       LNode slow = head;
+       LNode fast = head;
+       LNode prev = head;
+       while(fast != null && fast.next != null)
+       {
+           prev = slow;
+           slow = slow.next;
+           fast = fast.next.next;
+       }
+       if(prev.next != null)
+       prev.next = null;
+       return slow;
+   }
+}
