f26760db9a9f8910f6059622f2ceea0a2ceb7346
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-06-15 23:01:44 +0530

    Time: 77 ms (32.47%), Space: 58.7 MB (85.23%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -15,19 +15,20 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         int sum = 0;
         int m = -1;
-        
+        map.put(0,-1);
         for(int i = 0; i < nums.length; i++){
             sum += nums[i];
-            if(sum == target){
-                m = i + 1;
-            }
-            if(!map.containsKey(sum)){
-                map.put(sum, i);
-            }
+            // if(sum == target){
+            //     m = i + 1;
+            // }
+           
             if(map.containsKey(sum - target)){
                 if(m < (i - map.get(sum - target))){
                     m = i - map.get(sum - target);
                 }
+            }
+             if(!map.containsKey(sum)){
+                map.put(sum, i);
             }
         }
         return m != -1 ? nums.length - m : m;
