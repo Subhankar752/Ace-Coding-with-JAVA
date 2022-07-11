3d1c590c900ae560860d0b624010a1e5185ae22d
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-07-11 20:40:04 +0530

    Added solution - LeetHub

---
+++ Reaching the heights - GFG/reaching-the-heights.java
@@ -0,0 +1,90 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int N = Integer.parseInt(element[0]);
+		    int arr [] = new int[N];
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<N;i++){
+	            arr[i] = Integer.parseInt(elements[i]);    
+	        }
+		    
+		    
+		    Complete obj = new Complete();
+		    ArrayList<Integer> ans;
+		    ans = obj.reaching_height(N, arr);
+        	
+        	if(ans.size() == 1 && ans.get(0) == -1){
+        	    System.out.println("Not Possible");
+        	    continue;
+        	}
+        	
+        	for(int i: ans)
+        	    System.out.print(i + " ");
+        	System.out.println();
+		}
+	}
+}
+
+
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static ArrayList<Integer> reaching_height (int n, int arr[]) {
+        //Complete the function
+        Arrays.sort(arr);
+       ArrayList<Integer> ab=new ArrayList<>();
+       ArrayList<Integer> abc=new ArrayList<>();
+       if(n==1){
+           ab.add(arr[0]);
+           return ab;
+       }
+       for(int i=0;i<arr.length/2;i++){
+           ab.add(arr[n-i-1]);
+           ab.add(arr[i]);
+       }
+       if(n%2==1){
+           ab.add(arr[n/2]);
+       }
+       if(arr[n-1]==ab.get(n-1)){
+           abc.add(-1);
+           return abc;
+       }
+       return ab;
+    }
+    
+    
+}
