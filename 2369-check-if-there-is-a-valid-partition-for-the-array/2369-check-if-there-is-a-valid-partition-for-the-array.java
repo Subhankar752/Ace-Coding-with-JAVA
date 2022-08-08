ab823c13e071df83dc28ccbc6ab644688801e179
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-08-09 00:47:31 +0530

    Time: 21 ms (9.09%), Space: 99.6 MB (9.09%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,27 +1,25 @@
 class Solution {
-    Boolean dp[];
-    public boolean solve(int arr[] , int i){
-        if(i >= arr.length)   
-            return true;
-        
-        if(dp[i] != null) 
-            return dp[i];
-        if(i <= (arr.length - 3) && arr[i] == arr[i + 1] && arr[i + 1] == arr[i + 2]){
-            return dp[i] = solve(arr , i + 2) || solve(arr , i + 3);
-        }
-        if(i <= (arr.length - 2) && arr[i] == arr[i + 1]){
-            return  dp[i] = solve(arr , i + 2);
-        }
-        if(i <= (arr.length - 3)){
-            if((arr[i + 1] - arr[i]) == 1 && (arr[i + 2] - arr[i + 1]) == 1){
-                return dp[i] = solve(arr , i + 3);
-            }
-        }
-        System.out.println(i);
-        return false;
-    }
+    Boolean[] dp;
     public boolean validPartition(int[] nums) {
-        dp = new Boolean[nums.length];
+        dp = new Boolean[nums.length + 1];
         return solve(nums , 0);
+    }
+    public boolean solve(int[] nums , int i){
+        if(i >= nums.length)
+            return true;
+        if(dp[i] != null){
+            return dp[i];
+        }
+        if(i <= nums.length - 3 && nums[i] == nums[i + 1] && nums[i + 1] == nums[i + 2]){
+            return dp[i] = solve(nums , i + 2) | solve(nums , i + 3);
+        }
+        else if(i <= nums.length - 2 && nums[i] == nums[i + 1]){
+            return dp[i] = solve(nums , i + 2);
+        }
+        else if(i <= nums.length - 3 && nums[i] + 1 == nums[i + 1] && nums[i + 1] + 1 == nums[i + 2]){
+            return dp[i] = solve(nums , i + 3);
+        }
+        else
+            return false;
     }
 }
