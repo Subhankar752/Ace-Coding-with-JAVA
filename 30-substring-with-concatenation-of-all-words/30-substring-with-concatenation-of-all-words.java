851e942ecf716500720f5e0d840d16dad3832825
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-08-20 18:45:22 +0530

    Time: 230 ms (43.12%), Space: 118 MB (15.27%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> findSubstring(String S, String[] L) {
+        List<Integer> res = new ArrayList<Integer>();
+    if (S == null || L == null || L.length == 0) return res;
+    int len = L[0].length(); // length of each word
+    
+    Map<String, Integer> map = new HashMap<String, Integer>(); // map for L
+    for (String w : L) map.put(w, map.containsKey(w) ? map.get(w) + 1 : 1);
+    
+    for (int i = 0; i <= S.length() - len * L.length; i++) {
+        Map<String, Integer> copy = new HashMap<String, Integer>(map);
+        for (int j = 0; j < L.length; j++) { // checkc if match
+            String str = S.substring(i + j*len, i + j*len + len); // next word
+            if (copy.containsKey(str)) { // is in remaining words
+                int count = copy.get(str);
+                if (count == 1) copy.remove(str);
+                else copy.put(str, count - 1);
+                if (copy.isEmpty()) { // matches
+                    res.add(i);
+                    break;
+                }
+            } else break; // not in L
+        }
+    }
+        return res;
+    }
+}
