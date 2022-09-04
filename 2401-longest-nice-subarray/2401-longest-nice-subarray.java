2b13855fc461dec50d571119df5269790b352341
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-09-04 13:09:26 +0530

    Time: 5 ms (75.00%), Space: 76.8 MB (50.00%) - LeetHub

---
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int res = 1;
+        for(int i = 0; i < nums.length - res; i++) {
+            int first = nums[i];
+            int count = 1;
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((first & nums[j]) == 0) {
+                    first = first | nums[j];
+                    count++;
+                } else break;
+            }
+            res = Math.max(res, count);
+        }
+        return res;
+    }
+}
