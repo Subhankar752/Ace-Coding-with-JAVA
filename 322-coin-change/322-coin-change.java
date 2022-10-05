e4bd2c5f099950dd918f05af3072594b0fcb2866
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2022-10-05 11:57:13 +0530

    Time: 39 ms (38.02%), Space: 42.6 MB (87.28%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,18 +1,40 @@
 class Solution {
-    public int coinChange(int[] coins, int sum) {
-        return helper(coins , sum , new int[sum]);
+    
+    public int coinChange(int[] coins, int amount) {
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        
+        for(int i = 0 ; i < dp.length ; i++){
+            for(int j = 0 ; j < amount + 1 ; j++){
+                dp[i][j] = -1;
+            }
+        }
+        
+        int res = solve(coins , amount , 0 , dp);
+        
+        if(res == 12345678)
+            return -1;
+        
+        return res;
     }
-    public int helper(int[] coins , int rem , int[] count){
-        if(rem<0) return -1; // not valid
-    if(rem==0) return 0; // completed
-    if(count[rem-1] != 0) return count[rem-1]; // already computed, so reuse
-    int min = Integer.MAX_VALUE;
-    for(int coin : coins) {
-        int res = helper(coins, rem-coin, count);
-        if(res>=0 && res < min)
-            min = 1+res;
-    }
-    count[rem-1] = (min==Integer.MAX_VALUE) ? -1 : min;
-    return count[rem-1];
+    
+    public int solve(int[] coins , int rem , int idx , int[][] dp){
+        if(idx == coins.length - 1){
+            if(rem % coins[idx] == 0)
+                return rem/coins[idx];
+            else
+                return 12345678;
+        }
+        
+        if(dp[idx][rem] != -1)
+            return dp[idx][rem];
+        
+        int notTake = solve(coins , rem , idx + 1 , dp);
+        int take = Integer.MAX_VALUE;
+        
+        if(coins[idx] <= rem){
+            take = 1 + solve(coins , rem - coins[idx] , idx , dp);
+        }
+        
+        return dp[idx][rem] = Math.min(take , notTake);
     }
 }
