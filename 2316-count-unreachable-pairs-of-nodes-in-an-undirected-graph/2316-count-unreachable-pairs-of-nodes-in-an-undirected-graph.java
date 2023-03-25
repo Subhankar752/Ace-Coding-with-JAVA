03f388b199139cd4e003b36c6d51d19e62be1c2d
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-03-25 11:37:18 +0530

    Time: 80 ms (36.02%), Space: 128.5 MB (20.69%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -1,50 +1,53 @@
 class Solution {
-    ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
     public long countPairs(int n, int[][] edges) {
-        long ans = 0;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         
-        for(int i = 0 ; i < n ; i++)
+        long ans = 0;
+        for(int i = 0 ; i < n ; i++){
             adj.add(new ArrayList<>());
+        }
         
         for(int[] i : edges){
             int u = i[0];
             int v = i[1];
+            
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
         
         boolean[] vis = new boolean[n];
-        ArrayList<Integer> res = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         
         for(int i = 0 ; i < n ; i++){
             if(!vis[i]){
                 HashSet<Integer> hs = new HashSet<>();
-                dfs(i , vis , hs);
-                res.add(hs.size());
+                dfs(i , adj , hs , vis);
+                list.add(hs.size());
             }
         }
-        
+        //System.out.println(list);
         long sum = 0;
-        long curSum = 0;
-        for(int i : res)
+        for(int i : list){
             sum += i;
+        }
         
-        for(int i = 0 ; i < res.size() - 1 ; i++){
-            curSum += res.get(i);
-            ans += (sum - curSum) * res.get(i);
+        long curSum = 0;
+        
+        for(int i = 0 ; i < list.size() - 1 ; i++){
+            curSum += list.get(i);
+            ans += (sum - curSum) * list.get(i);
         }
         
         return ans;
     }
-    
-    public void dfs(int i , boolean[] vis , HashSet<Integer> hs){
-        
+    public void dfs(int i , ArrayList<ArrayList<Integer>> adj , HashSet<Integer> hs , boolean[] vis){
         vis[i] = true;
         hs.add(i);
         
         for(int ele : adj.get(i)){
-            if(!vis[ele])
-                dfs(ele , vis , hs);
+            if(!vis[ele]){
+                dfs(ele , adj , hs , vis);
+            }
         }
     }
 }
