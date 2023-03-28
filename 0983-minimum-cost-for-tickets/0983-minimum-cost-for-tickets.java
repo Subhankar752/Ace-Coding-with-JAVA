5c93b3dafb03f549918cf69fe99087821976ec49
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-03-28 21:02:59 +0530

    Time: 1 ms (77.14%), Space: 40.1 MB (78.89%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        int maxDay = days[days.length-1];
+        boolean [] travelDay = new boolean[maxDay+1];
+        
+        for(int day: days){
+            travelDay[day] =true;
+        }
+        
+        int [] dp = new int[maxDay+1];
+        dp[0]=0;
+        
+        for(int i=1;i<=maxDay;i++){
+            if(!travelDay[i]){
+                //dont have to buy a ticket if it is not a travel day
+                dp[i]=dp[i-1]; 
+                continue;
+            }
+            dp[i] = costs[0]+dp[i-1];
+            dp[i] = Math.min(dp[Math.max(0,i-7)]+costs[1],dp[i]);
+            dp[i] = Math.min(dp[Math.max(0,i-30)]+costs[2],dp[i]);
+        }
+        return dp[maxDay];
+    }
+}
