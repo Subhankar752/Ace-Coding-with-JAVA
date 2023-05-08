4a39181f8ed12a44adc1aad2671c62402bfe1a87
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-05-09 00:35:13 +0530

    Time: 23 ms (54.72%), Space: 42.1 MB (99.79%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -8,7 +8,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             count[i] = 1;
             pre[i] = -1;
-            for (int j = i - 1; j >= 0; j--) {
+            for (int j = 0; j < i; j++) {
                 if (nums[i] % nums[j] == 0) {
                     if (1 + count[j] > count[i]) {
                         count[i] = count[j] + 1;
