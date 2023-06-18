8e1351d6e22ab9a10823017e109398a267c09a92
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-06-19 00:01:54 +0530

    Time: 348 ms (5.31%), Space: 45.7 MB (5.05%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,25 +1,38 @@
 class Solution {
-   private static final int[][] dir = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+    private int[] d = {0, 1, 0, -1, 0};
+    
     public int minimumEffortPath(int[][] heights) {
-        int m = heights.length, n = heights[0].length;
-        int[][] dist = new int[m][n];
-        for(int i = 0; i < m; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
-        dist[0][0] = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
-        pq.add(new int[] {0, 0, 0});
-        while(!pq.isEmpty()) {
-            int[] p = pq.poll();
-            int i = p[0], j = p[1];
-            if(i == m - 1 && j == n - 1) break;
-            for(int[] d: dir) {
-                int x = i + d[0], y = j + d[1];
-                if(x < 0 || x >= m || y < 0 || y >= n) continue;
-                int alt = Math.max(p[2], Math.abs(heights[i][j] - heights[x][y]));
-                if(alt < dist[x][y]) {
-                    pq.add(new int[] {x, y, dist[x][y] = alt});
-                }
+        int lo = 0, hi = 1_000_000;
+        while (lo < hi) {
+            int effort = lo + (hi - lo) / 2;
+            if (isPath(heights, effort)) {
+                hi = effort;
+            }else {
+                lo = effort + 1;
             }
         }
-        return dist[m - 1][n - 1];
+        return lo;
+    }
+    
+    private boolean isPath(int[][] h, int effort) {
+        int m = h.length, n = h[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[2]);
+        Set<Integer> seen = new HashSet<>();
+        seen.add(0);
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0], y = cur[1];
+            if (x == m - 1 && y == n - 1) {
+                return true;
+            }
+            for (int k = 0; k < 4; ++k) {
+                int r = x + d[k], c = y + d[k + 1];
+                if (0 <= r && r < m && 0 <= c && c < n && effort >= Math.abs(h[r][c] - h[x][y]) && seen.add(r * n + c)) {
+                    q.offer(new int[]{r, c});
+                }
+            } 
+        }
+        return false;
     }
 }
