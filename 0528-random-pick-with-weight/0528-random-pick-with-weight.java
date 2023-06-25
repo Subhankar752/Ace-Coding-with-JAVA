fb22ef7cbd2f5f7b675ba1dbe69ac7352b7f366c
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-06-25 20:13:28 +0530

    Time: 31 ms (80.99%), Space: 47.5 MB (54.02%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    Random random;
+    int[] wSums;
+    
+    public Solution(int[] w) {
+        this.random = new Random();
+        for(int i=1; i<w.length; ++i)
+            w[i] += w[i-1];
+        this.wSums = w;
+    }
+    
+    public int pickIndex() {
+        int len = wSums.length;
+        int idx = random.nextInt(wSums[len-1]) + 1;
+        int left = 0, right = len - 1;
+        // search position 
+        while(left < right){
+            int mid = left + (right-left)/2;
+            if(wSums[mid] == idx)
+                return mid;
+            else if(wSums[mid] < idx)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+}
