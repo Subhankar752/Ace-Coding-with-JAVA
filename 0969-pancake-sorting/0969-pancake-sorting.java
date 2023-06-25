7e6dba9d4aa5c6dab113e41d07170800a4e152e9
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-06-25 23:00:20 +0530

    Time: 1 ms (99.31%), Space: 41 MB (100.00%) - LeetHub

---
+++ 0969-pancake-sorting/0969-pancake-sorting.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public List<Integer> pancakeSort(int[] A) {
+        List<Integer> res = new ArrayList<>();
+        
+        for (int x = A.length, i; x > 0; --x) {
+            for (i = 0; A[i] != x; ++i);
+            reverse(A, i + 1);
+            res.add(i + 1);
+            reverse(A, x);
+            res.add(x);
+        }
+        
+        return res;
+    }
+
+    public void reverse(int[] A, int k) {
+        for (int i = 0, j = k - 1; i < j; i++, j--) {
+            int tmp = A[i];
+            A[i] = A[j];
+            A[j] = tmp;
+        }
+    }
+}
