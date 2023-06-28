06bac244033c1003ff68795a4758ef642e771a1c
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-06-28 22:55:53 +0530

    Time: 10 ms (60.78%), Space: 41.7 MB (80.55%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int mostFreqLetter = 0;
+        int left = 0;
+        int max = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            freq[s.charAt(right) - 'A']++;
+            mostFreqLetter = Math.max(mostFreqLetter, freq[s.charAt(right) - 'A']);
+
+            int lettersToChange = (right - left + 1) - mostFreqLetter;
+            
+            if (lettersToChange > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            max = Math.max(max, right - left + 1);
+        }
+
+        return max;
+    }
+}
