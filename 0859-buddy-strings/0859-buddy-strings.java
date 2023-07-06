d922d58fc0fb7b65ed5d8aa15351dc9b6ab6feda
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-07-06 23:41:36 +0530

    Time: 4 ms (53.31%), Space: 41.4 MB (88.73%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    public boolean buddyStrings(String A, String B) {
+        if (A.length() != B.length()) return false;
+        if (A.equals(B)) {
+            Set<Character> s = new HashSet<Character>();
+            for (char c : A.toCharArray()) s.add(c);
+            return s.size() < A.length();
+        }
+        List<Integer> dif = new ArrayList<>();
+        for (int i = 0; i < A.length(); ++i) if (A.charAt(i) != B.charAt(i)) dif.add(i);
+        return dif.size() == 2 && A.charAt(dif.get(0)) == B.charAt(dif.get(1)) && A.charAt(dif.get(1)) == B.charAt(dif.get(0));
+    }
+}
