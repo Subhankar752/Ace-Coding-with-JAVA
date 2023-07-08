a23ae89f5e73f6ed7ad7544b16dcea82368fbf7c
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-07-08 13:09:25 +0530

    Time: 35 ms (74.75%), Space: 41.7 MB (79.80%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -1,33 +1,34 @@
 class Solution {
+
     public int largestOverlap(int[][] img1, int[][] img2) {
-        int ans=0;
-        for(int i=0;i<img1.length;i++) {
-            for(int j=0;j<img1.length;j++) {
-                ans = Math.max(ans,rightDownLeftUp(img1,img2,i,j));
-                ans = Math.max(ans,rightDownLeftUp(img2,img1,i,j));
-                
-                ans = Math.max(ans,rightUpLeftDown(img1,img2,i,j));
-                ans = Math.max(ans,rightUpLeftDown(img2,img1,i,j));
+        int ans = 0;
+        for (int i = 0; i < img1.length; i++) {
+            for (int j = 0; j < img1.length; j++) {
+                ans = Math.max(ans, rightDownLeftUp(img1, img2, i, j));
+                ans = Math.max(ans, rightDownLeftUp(img2, img1, i, j));
+
+                ans = Math.max(ans, rightUpLeftDown(img1, img2, i, j));
+                ans = Math.max(ans, rightUpLeftDown(img2, img1, i, j));
             }
         }
         return ans;
     }
-    
-    public static int rightDownLeftUp(int[][] a,int[][] b,int iShift,int jShift) {
-        int count=0;
-        for(int i=iShift;i<a.length;i++) {
-            for(int j=jShift;j<a.length;j++) {
-                if(a[i][j]==1 && b[i-iShift][j-jShift]==1) count++;
+
+    public static int rightDownLeftUp(int[][] a, int[][] b, int iShift, int jShift) {
+        int count = 0;
+        for (int i = iShift; i < a.length; i++) {
+            for (int j = jShift; j < a.length; j++) {
+                if (a[i][j] == 1 && b[i - iShift][j - jShift] == 1) count++;
             }
         }
         return count;
     }
-    
-    public static int rightUpLeftDown(int[][] a,int[][] b,int iShift,int jShift) {
-        int count=0;
-        for(int i=iShift;i<a.length;i++) {
-            for(int j=0;j<a.length-jShift;j++) {
-                if(a[i][j]==1 && b[i-iShift][j+jShift]==1) count++;
+
+    public static int rightUpLeftDown(int[][] a, int[][] b, int iShift, int jShift) {
+        int count = 0;
+        for (int i = iShift; i < a.length; i++) {
+            for (int j = 0; j < a.length - jShift; j++) {
+                if (a[i][j] == 1 && b[i - iShift][j + jShift] == 1) count++;
             }
         }
         return count;
