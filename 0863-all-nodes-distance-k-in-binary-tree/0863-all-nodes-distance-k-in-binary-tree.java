c35f1aca0cc93031fdd36dfe688862438ba44403
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-07-08 13:46:08 +0530

    Time: 13 ms (44.65%), Space: 42 MB (50.37%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    Map<TreeNode, List<TreeNode>> map = new HashMap();
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null || K < 0) return res;
+        buildMap(root, null);
+        if (!map.containsKey(target)) return res;
+        Set<TreeNode> visited = new HashSet<TreeNode>();
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(target);
+        visited.add(target);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            if (K == 0) {
+                for (int i = 0; i < size; i++) res.add(q.poll().val);
+                return res;
+            }
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                for (TreeNode next : map.get(node)) {
+                    if (visited.contains(next)) continue;
+                    visited.add(next);
+                    q.add(next);
+                }
+            }
+            K--;
+        }
+        return res;
+    }
+
+    private void buildMap(TreeNode node, TreeNode parent) {
+        if (node == null) return;
+        if (!map.containsKey(node)) {
+            map.put(node, new ArrayList<TreeNode>());
+            if (parent != null) {
+                map.get(node).add(parent);
+                map.get(parent).add(node);
+            }
+            buildMap(node.left, node);
+            buildMap(node.right, node);
+        }
+    }
+}
