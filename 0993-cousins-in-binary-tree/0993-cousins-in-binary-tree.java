9eed378453bb000d66697c5a72c90891771712ec
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-07-09 13:57:29 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (78.62%) - LeetHub

---
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int A, int B) {
+        if (root == null) return false;
+	Queue<TreeNode> queue = new LinkedList<>();
+	queue.offer(root);
+	while (!queue.isEmpty()) {
+		int size = queue.size();
+		boolean isAexist = false;		
+		boolean isBexist = false;		
+		for (int i = 0; i < size; i++) {
+			TreeNode cur = queue.poll();
+            if (cur.val == A) isAexist = true;
+            if (cur.val == B) isBexist = true;
+			if (cur.left != null && cur.right != null) { 
+				if (cur.left.val == A && cur.right.val == B) { 
+					return false;
+				}
+				if (cur.left.val == B && cur.right.val == A) { 
+					return false;
+				}
+			}
+			if (cur.left != null) {
+				queue.offer(cur.left);
+			}
+			if (cur.right != null) {
+				queue.offer(cur.right);
+			}
+		}
+		if (isAexist && isBexist)  return true;
+	}
+	return false;
+    }
+}
