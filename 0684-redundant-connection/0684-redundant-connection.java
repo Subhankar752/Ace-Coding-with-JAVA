c78921d66f09dc58986e72b706c47e523a081bcb
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-07-18 00:48:55 +0530

    Time: 1 ms (73.72%), Space: 43.6 MB (57.60%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,24 +1,24 @@
 class Solution {
+
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
-        
+
         DSU dsu = new DSU(n);
-        
-        for(int[] edge : edges){
+
+        for (int[] edge : edges) {
             int nodeX = edge[0] - 1;
             int nodeY = edge[1] - 1;
-            
-            
+
             int parentX = dsu.find(nodeX);
             int parentY = dsu.find(nodeY);
-            
-            if(parentX == parentY){
+
+            if (parentX == parentY) {
                 return edge;
             }
-            
+
             dsu.unionSet(nodeX, nodeY);
         }
-        
+
         return new int[0];
     }
 }
@@ -26,43 +26,37 @@ class Solution {
 class DSU {
     int[] parent;
     int[] rank;
-    
-    public DSU(int size){
+
+    public DSU(int size) {
         parent = new int[size];
         rank = new int[size];
-        
-        for(int i = 0; i < size; i++){
+
+        for (int i = 0; i < size; i++) {
             parent[i] = i;
             rank[i] = 0;
         }
     }
-    
-    public int find(int x){
-        if(x == parent[x])
+
+    public int find(int x) {
+        if (x == parent[x]) 
             return x;
-        
+
         parent[x] = find(parent[x]);
-        
+
         return parent[x];
     }
-    
-    public void unionSet(int x, int y){
-        
+
+    public void unionSet(int x, int y) {
         int u = find(x);
         int v = find(y);
-        
-        if(rank[u] < rank[v]){
+
+        if (rank[u] < rank[v]) {
             parent[u] = v;
-        }
-        else if(rank[v] < rank[u]){
+        } else if (rank[v] < rank[u]) {
             parent[v] = u;
-        }
-        else{
+        } else {
             parent[u] = v;
-            v++;
+            rank[v]++;
         }
-        
     }
-    
-    
 }
