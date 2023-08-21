70f2b90ac4bbf72629f919f20677fee3b8f0480b
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-08-21 22:59:48 +0530

    Time: 11 ms (44.72%), Space: 41.4 MB (80.71%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -5,7 +5,8 @@ class Solution {
         int mostFreqLetter = 0;
         int left = 0;
         int max = 0;
-
+        
+        
         for (int right = 0; right < s.length(); right++) {
             freq[s.charAt(right) - 'A']++;
             mostFreqLetter = Math.max(mostFreqLetter, freq[s.charAt(right) - 'A']);
