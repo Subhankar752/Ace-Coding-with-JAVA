000f26074f1196899ceb2cbbf0eddd4be2d5d6a4
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2023-09-02 23:15:55 +0530

    Time: 3 ms (80.12%), Space: 41 MB (83.95%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,35 +1,22 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        // put all words into a hashset
-        // Set<String> set = new HashSet<>(wordDict);
-        // return wb(s, set);
-        int n = s.length();
-        boolean[] dp = new boolean[n + 1];
-        dp[0] = true;
-        
-        Set<String> wordSet = new HashSet<>(wordDict);
-        
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && wordSet.contains(s.substring(j, i))) {
-                    dp[i] = true;
-                    break;
+        boolean[] dp = new boolean[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            for (String word: wordDict) {
+                // Handle out of bounds case
+                if (i < word.length() - 1) {
+                    continue;
+                }
+                
+                if (i == word.length() - 1 || dp[i - word.length()]) {
+                    if (s.substring(i - word.length() + 1, i + 1).equals(word)) {
+                        dp[i] = true;   
+                        break;
+                    }
                 }
             }
         }
         
-        return dp[n];
-    }
-    private boolean wb(String s, Set<String> set) {
-        int len = s.length();
-        if (len == 0) {
-            return true;
-        }
-        for (int i = 1; i <= len; ++i) {
-            if (set.contains(s.substring(0, i)) && wb(s.substring(i), set)) {
-                return true;
-            }
-        }
-        return false;
+        return dp[s.length() - 1];
     }
 }
