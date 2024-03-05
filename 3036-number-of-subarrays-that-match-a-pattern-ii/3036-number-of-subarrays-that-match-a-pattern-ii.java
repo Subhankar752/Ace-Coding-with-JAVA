7530a04d1ad481c2ceafcd3a8d11f556fa318afb
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2024-03-05 16:13:36 +0530

    Time: 139 ms (5.11%), Space: 173.8 MB (31.26%) - LeetHub

---
+++ 3036-number-of-subarrays-that-match-a-pattern-ii/3036-number-of-subarrays-that-match-a-pattern-ii.java
@@ -0,0 +1,57 @@
+class Solution {
+
+    public int countMatchingSubarrays(int[] nums, int[] pattern) {
+        int ans = 0;
+
+        int n = nums.length;
+        StringBuilder s = new StringBuilder();
+        StringBuilder p = new StringBuilder();
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                s.append("2");
+            } else if (nums[i] == nums[i + 1]) {
+                s.append("0");
+            } else {
+                s.append("1");
+            }
+        }
+
+        for (int i : pattern) {
+            if (i == -1) {
+                p.append("2");
+            } else if (i == 0) {
+                p.append("0");
+            } else {
+                p.append("1");
+            }
+        }
+
+        String str = p.toString() + "#" + s.toString();
+        int[] lps = kmp(str);
+        System.out.println(Arrays.toString(lps));
+
+        int len = pattern.length;
+        for (int i : lps) {
+            if (i == len) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+
+    public int[] kmp(String s) {
+        int[] lps = new int[s.length()];
+        
+        for (int i = 1; i < s.length(); i++) {
+            int prevIdx = lps[i - 1];
+            while (prevIdx > 0 && s.charAt(i) != s.charAt(prevIdx)) {
+                prevIdx = lps[prevIdx - 1];
+            }
+            lps[i] = prevIdx + (s.charAt(i) == s.charAt(prevIdx) ? 1 : 0);
+        }
+
+        return lps;
+    }
+}
