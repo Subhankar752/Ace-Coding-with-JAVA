7d372bc5463be6296c8339eef44fdb4137e0d3b2
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2024-05-03 00:43:08 +0530

    Time: 722 ms (25.41%), Space: 133.1 MB (31.22%) - LeetHub

--- 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.java
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.java
@@ -1,34 +1,48 @@
 class Solution {
-    static int l;
-    static int dp[][][][];
-    static int mod = 1000000007;
-
-    static int helper(int zero, int one, int conseq, int prev) {
-        if (dp[zero][one][prev][conseq] != -1) return dp[zero][one][prev][conseq] % mod;
-
-        int ans = 0;
-        if (zero == 0 && one == 0) return 1;
-        if (zero > 0) {
-            if (prev == 0 && conseq < l) {
-                ans = ans + helper(zero - 1, one, conseq + 1, 0);
-            }
-            if (prev == 1) ans = ans + helper(zero - 1, one, 1, 0);
-        }
-        if (one > 0) {
-            if (prev == 1 && conseq < l) {
-                ans = ans + helper(zero, one - 1, conseq + 1, 1);
-            }
-            if (prev == 0) ans = ans + helper(zero, one - 1, 1, 1);
-        }
-        return dp[zero][one][prev][conseq] = ans % mod;
-    }
+    int[][][][] dp;
+    int mod = (int) (1e9 + 7);
 
     public int numberOfStableArrays(int zero, int one, int limit) {
-        l = limit;
-        dp = new int[zero + 1][one + 1][2][l + 1];
-        for (var a : dp) for (var b : a) for (var c : b) Arrays.fill(c, -1);
-        int a = helper(zero - 1, one, 1, 0) % mod;
-        int b = helper(zero, one - 1, 1, 1) % mod;
-        return (a + b) % mod;
+        dp = new int[zero + 1][one + 1][2][limit + 1];
+        for (int i = 0; i < zero + 1; i++) {
+            for (int j = 0; j < one + 1; j++) {
+                for (int k = 0; k < 2; k++) {
+                    for (int l = 0; l < limit + 1; l++) {
+                        dp[i][j][k][l] = -1;
+                    }
+                }
+            }
+        }
+        return (solve(zero - 1, one, limit, 0, 1) % mod + solve(zero, one - 1, limit, 1, 1) % mod) % mod;
+    }
+
+    public int solve(int zero, int one, int limit, int prev, int cons) {
+        if (dp[zero][one][prev][cons] != -1) {
+            return dp[zero][one][prev][cons];
+        }
+
+        if (zero == 0 && one == 0) {
+            return 1;
+        }
+
+        int ans = 0;
+
+        if (zero > 0) {
+            if (prev == 0 && cons < limit) {
+                ans = (ans % mod + solve(zero - 1, one, limit, prev, cons + 1) % mod) % mod;
+            } else if (prev == 1) {
+                ans = (ans % mod + solve(zero - 1, one, limit, 0, 1) % mod) % mod;
+            }
+        }
+
+        if (one > 0) {
+            if (prev == 1 && cons < limit) {
+                ans = (ans % mod + solve(zero, one - 1, limit, prev, cons + 1) % mod) % mod;
+            } else if (prev == 0) {
+                ans = (ans % mod + solve(zero, one - 1, limit, 1, 1) % mod) % mod;
+            }
+        }
+
+        return dp[zero][one][prev][cons] = ans;
     }
 }
