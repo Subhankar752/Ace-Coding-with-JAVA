a76f7602c0b2559ace2af7572d0a0d8d39dc0377
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2024-05-12 20:33:35 +0530

    Time: 463 ms (87.50%), Space: 44.9 MB (75.00%) - LeetHub

---
+++ 3144-minimum-substring-partition-of-equal-character-frequency/3144-minimum-substring-partition-of-equal-character-frequency.java
@@ -0,0 +1,49 @@
+class Solution {
+    int[] dp;
+
+    public int minimumSubstringsInPartition(String s) {
+        int n = s.length();
+        int ans = n;
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        ans = Math.min(ans, solve(0, s, n));
+        return ans;
+    }
+
+    public int solve(int i, String s, int n) {
+        if (i >= n) {
+            return 0;
+        }
+
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        int[] freq = new int[26];
+        int ans = n;
+        for (int j = i; j < n; j++) {
+            char ch = s.charAt(j);
+            freq[ch - 'a']++;
+
+            if (balanced(freq)) {
+                ans = Math.min(ans, 1 + solve(j + 1, s, n));
+            }
+        }
+
+        return dp[i] = ans;
+    }
+
+    public boolean balanced(int[] freq) {
+        int maxfreq = -1;
+        int minfreq = 5005;
+        for (int i : freq) {
+            if (i > 0) {
+                minfreq = Math.min(minfreq, i);
+                maxfreq = Math.max(maxfreq, i);
+            }
+        }
+
+        return maxfreq == minfreq;
+    }
+}
