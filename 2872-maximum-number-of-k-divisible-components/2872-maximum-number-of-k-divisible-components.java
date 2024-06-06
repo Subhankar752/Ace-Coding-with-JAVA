09d544404424916aea7b4b2ae62685b5ac72d238
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2024-06-06 21:23:18 +0530

    Time: 91 ms (31.94%), Space: 59.8 MB (97.22%) - LeetHub

---
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.java
@@ -0,0 +1,44 @@
+class Solution {
+    List<List<Integer>> adj;
+    int ans = 0;
+    int[] values;
+
+    public int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
+        adj = new ArrayList<>();
+        ans = 0;
+        this.values = values;
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        System.out.println(adj);
+
+        // solve(0, -1, k);
+        solve(0, -1, k);
+        return ans;
+    }
+
+    public long solve(int node, int parent, int k) {
+        long sum = values[node];
+
+        for (int ele : adj.get(node)) {
+            if (ele != parent) {
+                sum += solve(ele, node, k);
+            }
+        }
+
+        if (sum % k == 0) {
+            ans++;
+            sum = 0;
+        }
+
+        return sum;
+    }
+}
