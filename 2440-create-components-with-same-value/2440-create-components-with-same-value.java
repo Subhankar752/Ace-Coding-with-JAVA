7a8707ad81d061ee788df2cc48dae6ff83adbb1b
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2024-06-07 12:07:41 +0530

    Time: 290 ms (6.82%), Space: 62.2 MB (6.82%) - LeetHub

---
+++ 2440-create-components-with-same-value/2440-create-components-with-same-value.java
@@ -0,0 +1,62 @@
+class Solution {
+    List<List<Integer>> adj;
+    long res;
+    int components;
+    int val;
+
+    public int componentValue(int[] nums, int[][] edges) {
+        int sum = 0;
+        int ans = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+
+        adj = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] i : edges) {
+            int u = i[0];
+            int v = i[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        for (int i = 1; i <= sum; i++) {
+            if (sum % i == 0) {
+                components = i;
+                val = sum / i;
+                solve(0, -1, nums);
+                if (components == 0) {
+                    ans = Math.max(ans, i - 1);
+                }
+
+                // components = sum / i;
+                // val = i;
+                // solve(0, -1, nums);
+                // if (components == 0) {
+                //     ans = Math.max(ans, sum / i - 1);
+                // }
+            }
+        }
+
+        return ans;
+    }
+
+    public int solve(int node, int parent, int[] nums) {
+        int sum = nums[node];
+        for (int ele : adj.get(node)) {
+            if (ele != parent) {
+                sum += solve(ele, node, nums);
+            }
+        }
+
+        if (sum == val) {
+            components--;
+            sum = 0;
+        }
+
+        return sum;
+    }
+}
