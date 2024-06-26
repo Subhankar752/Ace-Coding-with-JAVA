84755c85e1af78a1a43194afcdfafcb6a6e4b57f
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2024-06-26 22:31:53 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (81.95%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,13 @@
+public class Solution {
+    private static final int MOD = 1000000007;
+
+    public int countOrders(int n) {
+        long count = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            count = (count * (2 * i - 1) * i) % MOD;
+        }
+        
+        return (int) count;
+    }
+}
