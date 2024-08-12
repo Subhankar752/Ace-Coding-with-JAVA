3fe4cc436ab44610904cfcca2345a2a9853e0430
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2024-08-12 21:01:33 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (98.48%) - LeetHub

--- 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.java
+++ 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.java
@@ -6,8 +6,8 @@ class Solution {
     }
     
     private int gcd(int a, int b) {
-        if (a == 0) 
-            return b;
-        return gcd(b % a, a);
+        if (b == 0) 
+            return a;
+        return gcd(b, a % b);
     }
 }
