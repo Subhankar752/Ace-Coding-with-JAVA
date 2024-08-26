48db799b4d081103111fab77854a7d62c12c7db0
Subhankar Majumdar <57652251+Subhankar752@users.noreply.github.com>
2024-08-26 21:00:54 +0530

    Time: 27 ms (92.96%), Space: 64.9 MB (5.07%) - LeetHub

---
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    class Pair {
+        int idx1;
+        int idx2;
+        int val;
+
+        Pair(int idx1, int idx2, int val) {
+            this.idx1 = idx1;
+            this.idx2 = idx2;
+            this.val = val;
+        }
+    }
+
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        List<List<Integer>> ans = new ArrayList<>();
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int i = 0;
+        int j = 0;
+        
+        pq.add(new Pair(i, j, nums1[i] + nums2[j]));
+        while (pq.size() > 0 && k-- > 0) {
+            Pair p = pq.remove();
+            List<Integer> list = new ArrayList<>();
+            list.add(nums1[p.idx1]);
+            list.add(nums2[p.idx2]);
+            ans.add(list);
+            
+            //next column
+            if (p.idx2 == 0 && p.idx1 + 1 < n1) {
+                pq.add(new Pair(p.idx1 + 1, p.idx2, nums1[p.idx1 + 1] + nums2[p.idx2]));
+            }
+            //next row
+            if (p.idx2 + 1 < n2) {
+                pq.add(new Pair(p.idx1, p.idx2 + 1, nums1[p.idx1] + nums2[p.idx2 + 1]));
+            }
+        }
+
+        return ans;
+    }
+}
